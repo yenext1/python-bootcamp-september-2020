ae401b08145ab4428591a93f4a53afcb83844c4e
Uri <uri@speedy.net>
2020-09-08 14:05:57 +0300

    Uri's comments to exercise 5.

--- lab5/exercise_1.py
+++ lab5/exercise_1.py
@@ -2,3 +2,22 @@
 age_in_years = input("How old are you? (in years)")
 age_in_months = round(float(age_in_years)*12)
 print(f"You are {age_in_months} months old!")
+
+"""
+Uri's comments:
+==============
+
+* Very good! This code works.
+* It's better to add a space in the input after "(in years)", so that the user's
+  input will be separated from the last word of the input.
+  This is relevant to all your exercises.
+* I recommend styling your code according to PEP-8. You can also use PyCharm's
+  Code -> Reformat Code feature. For example, spaces around "*".
+  This is relevant to all your exercises.
+* Here is how your code looks reformatted:
+
+age_in_years = input("How old are you? (in years)")
+age_in_months = round(float(age_in_years) * 12)
+print(f"You are {age_in_months} months old!")
+  
+"""
--- lab5/exercise_5.py
+++ lab5/exercise_5.py
@@ -6,3 +6,22 @@ n = int(input("How many numbers should we sum? (n)"))
 sum = (n/2)*(2*a1+(n-1)*d)
 
 print(f" The sum of the numbers in the serie is {sum}")
+
+"""
+Uri's comments:
+==============
+
+* Very good! This code works.
+* This is sub-exercise 6.
+* "choose the number you and to add" is not the correct way to write in English.
+* The input is done in int, but the result is in float. Maybe it's better to
+  convert it to int (it can't be a non-integer number).
+  You can also do the whole calculation in int to reserve precision (you lose 
+  precision after the first ~15 digits when you convert an int to a float).
+* sum is a built-in function in Python and although you can, it's not recommended
+  to use it as a name of a variable.
+  PyCharm warns about it (you can see a line under "sum" which you can hover and 
+  see the full warning).
+* There are 6 sub-exercises in exercise 5, and you only did 5 of them. 
+
+"""
