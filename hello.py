e68f2105d479d0ab6dff95b2108d4d4851e4a28b
Yonatan <example@example.com>
2020-09-24 20:22:41 +0300

    selenium start

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,4 +8,34 @@ soup = BeautifulSoup(r.text)
 for link in soup.select('a.smallheader'):
     print(link.text)
 
+
+num = int(input("Choose a number"))
+
+def raise_unless(num):
+    if num % 3 != 0:
+        raise Exception("Hahahah")
+
+
+
+
+try:
+    raise_unless(num)
+except Exception:
+    print("Nice")
+
+
+
+class LightBuld:
+    def __init__(self):
+        self.is_lights_on = False
+
+    def light_on(self):
+        if not self.is_lights_on:
+            print("Lights On")
+            self.is_lights_on = True
+
+    def light_off(self):
+        if self.is_lights_on:
+            print("Lights Off")
+            self.is_lights_on = False
 
